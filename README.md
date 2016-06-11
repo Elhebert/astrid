02cb9897a47109b01ab01da868bcde224fcde79d
Elhebert <elhebert@hotmail.be>
2016-06-11 23:35:57 +0200

    Inial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Astrid
+
+This is Astrid a light **ES6 javascript** module to create sticky elements.
+
+Why Astrid ? Astrid is the seestar from Nemo. And everone knows a seestar is sticky.
+![astrid](https://media.giphy.com/media/ulSIu5DqlV51C/giphy.gif)
+
+Special thanks to [Grafikart](https://github.com/grafikart). This script is an ES6 adaption of his tutorial about [sticky elements](https://www.grafikart.fr/formations/debuter-javascript/menu-collant).
+
+## Usage
+
+Add the **astrid.js** file to your index.(html|php).
+```
+<script src="/path/to/astrid.js"></script>
+```
+
+Add `data-sticky` on the html element that need to be sticky. When the element is in sticky mode, it'll have the css class `sticky`.
+You can thus, customize it.
+
+
+## Options
+
+### Offset
+Add an offset to the element.
+
+The offset will define the distance (in pixel) between the top of the page and the element.
+
+Usage:
+```
+<my-element data-offset="50"></my-element>
+```
+
+### Constraint
+Add a constraint to the element.
+
+The constraint will define within which element (using the css class) the sticky one will be constriced. It won't be able to go further that element.
+
+Usage:
+```
+<my-parent-element class="constraint-element">
+	<my-element data-sticky data-constraint=".constraint-element"></my-element>
+</my-parent-element>
+```
---
+++ astrid.js
@@ -0,0 +1,99 @@
+{
+
+	'use strict';
+
+	/**
+	 * ScrollY polyfill for old navigator versions. (eg. IE < 9)
+	 * @see https://developer.mozilla.org/en/docs/Web/API/Window/scrollY
+	 */
+	let scrollY = () => {
+		let supportPageOffset = window.pageXOffset !== undefined;
+		let isCSS1Compat = ((document.compatMode || '')) === 'CSS1Compat';
+		return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+	};
+
+	/**
+	 * Make an element sticky
+	 * @param {element} element that need to be fixed to the top of the page.
+	 */
+	let makeSticky = (element) => {
+		let rect = element.getBoundingClientRect();
+		let offset = parseInt(element.getAttribute('data-offset') || 0, 10);
+		let constraint = (element.getAttribute('data-constraint')
+							&& element.querySelector(element.getAttribute('data-constraint')))
+							|| document.body;
+		let constraintRect = constraint.getBoundingClientRect();
+		let constraintBottom = constraintRect.top + scrollY() + constraintRect.height - offset - rect.height;
+		let top = rect.top + scrollY();
+		let fake = document.createElement('div');
+
+		fake.style.width = rect.width + 'px';
+		fake.style.height = rect.height + 'px';
+
+		/**
+		 * Triggered every a scroll event is detected on the page.
+		 */
+		let onScroll = () => {
+			let hasScrollClass = element.classList.contains('sticky');
+			if (scrollY() > constraintBottom && element.style.position !== 'absolute') {
+				element.classList.remove('sticky');
+
+				element.style.position = 'absolute';
+				element.style.top = 'auto';
+				element.style. bottom = '0';
+			} else if (
+				scrollY() > (top - offset)
+				&& scrollY() < constraintBottom
+				&& !hasScrollClass && element.style.position !== 'fixed'
+			) {
+				element.classList.add('sticky');
+
+				element.style.position = 'fixed';
+				element.style.top = offset + 'px';
+				element.style.bottom = 'auto';
+				element.style.width = rect.width + 'px';
+
+				element.parentNode.insertBefore(fake, element);
+			} else if (scrollY() < (top - offset) && hasScrollClass && element.style.position !== 'relative') {
+				element.classList.remove('sticky');
+
+				element.style.position = 'relative';
+
+				element.parentNode.contains(fake) && element.parentNode.removeChild(fake);
+			}
+		};
+
+		/**
+		 * Triggered every time a resize event is detected on the pafe
+		 */
+		let onResize = () => {
+			element.classList.remove('sticky');
+
+			element.style.position = 'relative';
+			element.style.width = 'auto';
+			fake.style.display = 'none';
+
+			rect = element.getBoundingClientRect();
+			top = rect.top + scrollY();
+
+			fake.style.width = rect.width + 'px';
+			fake.style.height = rect.height + 'px';
+			fake.style.display = 'block';
+
+			onScroll();
+		};
+
+		window.addEventListener('scroll', onScroll);
+		window.addEventListener('resize', onResize);
+	};
+
+	/**
+	 * Make every element with the [data-sticky] attribute sticky.
+	 */
+	let elements = document.querySelectorAll('[data-sticky]');
+	for (let key in elements) {
+		if (elements.hasOwnProperty(key)) {
+			makeSticky(elements[key]);
+		}
+	};
+}
